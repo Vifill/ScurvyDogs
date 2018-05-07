b11d2ef6ad0495396099ca88318185f0cef86ea3
Theodor Te Maiharoa <tedditm@gmail.com>
2018-05-07 14:01:35 +0200

    new versus scene to test selection on main menu

--- Assets/MenuUIManager.cs
+++ Assets/MenuUIManager.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuUIManager : NetworkBehaviour
 {
-
+    public Dropdown GameModeDropDown;
     private NetworkManager NetworkManager;
+
+    public void SelectGameMode()
+    {
+        NetworkManager = FindObjectOfType<NetworkManager>();
+
+        if (GameModeDropDown.value == 0)
+        {
+            //coop
+            var scene = SceneManager.GetSceneByBuildIndex(1).name;
+            NetworkManager.onlineScene = scene;
+        }
+        else if (GameModeDropDown.value == 1)
+        {
+            //Versus
+            var scene = SceneManager.GetSceneByBuildIndex(2).name;
+            NetworkManager.onlineScene = scene;
+        }
+    }
 
     public void ButtonStartHost()
     {
